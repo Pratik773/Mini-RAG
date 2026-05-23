f551d4e4b32d8a0ff7f3a06af3bbaace2cad7031
git3 <pratiknerkar04@gmail.com>
2026-05-23 19:54:44 +0530

    deployment setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -54,41 +54,43 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <h1>Mini RAG AI</h1>
+    <div class="a">
+      <div className="container">
+        <h1>Mini RAG AI</h1>
 
-      {/* Upload Section */}
-      <div className="card">
-        <h2>Upload PDF</h2>
+        {/* Upload Section */}
+        <div className="card">
+          <h2>Upload PDF</h2>
 
-        <input
-          type="file"
-          accept=".pdf"
-          onChange={(e) => setFile(e.target.files[0])}
-        />
+          <input
+            type="file"
+            accept=".pdf"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
 
-        <button onClick={uploadFile}>Upload</button>
-      </div>
+          <button onClick={uploadFile}>Upload</button>
+        </div>
 
-      {/* Question Section */}
-      <div className="card">
-        <h2>Ask Question</h2>
+        {/* Question Section */}
+        <div className="card">
+          <h2>Ask Question</h2>
 
-        <input
-          type="text"
-          placeholder="Ask something from PDF..."
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-        />
+          <input
+            type="text"
+            placeholder="Ask something from PDF..."
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+          />
 
-        <button onClick={askQuestion}>Ask</button>
-      </div>
+          <button onClick={askQuestion}>Ask</button>
+        </div>
 
-      {/* Answer Section */}
-      <div className="card">
-        <h2>AI Answer</h2>
+        {/* Answer Section */}
+        <div className="card">
+          <h2>AI Answer</h2>
 
-        <p>{answer}</p>
+          <p>{answer}</p>
+        </div>
       </div>
     </div>
   );
